e957438189f828dcdeba4652de93568b5fa48657
Carolina de Faria Paulo <carolinadefpaulo@gmail.com>
2021-03-21 18:08:14 -0300

    feat: new exercise

---
+++ Intro to Programing/Arrays/Factors_Of.rb
@@ -0,0 +1,25 @@
+#Factors Of
+#Write a method factors_of(num) that takes in a num and returns an array of all positive numbers less than or equal to num that can divide num.
+
+def factors_of(num)
+	newArray = []
+  	i = 1
+  	while i <= num
+    	if num % i == 0
+          newArray << i
+        end
+      i += 1
+    end
+  return newArray
+  	
+end
+
+print factors_of(3)   # => [1, 3]
+puts
+print factors_of(4)   # => [1, 2, 4]
+puts
+print factors_of(8)   # => [1, 2, 4, 8]
+puts
+print factors_of(9)   # => [1, 3, 9]
+puts
+print factors_of(16)  # => [1, 2, 4, 8, 16]
