84ac3a123d8c7db50cee9dc8e556accc4a43eeee
Carolina de Faria Paulo <carolinadefpaulo@gmail.com>
2021-03-20 22:34:25 -0300

    feat: new exercise

---
+++ Introduction/First_Program.rb
@@ -0,0 +1,8 @@
+#First Program
+#Write your first program! Using puts, print out a message. Make sure that your message is wrapped up in quotation marks. Run the code to see the output. Note: You'll notice that one of the lines in the code begins with a #. 
+#Adding # to the front of a line will create a comment. This line will not be executed as code, so they are useful to write notes to ourselves!
+
+puts "Hello World!"
+
+# Print out another message using puts below:
+puts "hey, nice to be here!" #another line 
