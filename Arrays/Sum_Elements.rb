b7efa690b273066d7464b96a78da95d230f6f0da
Carolina de Faria Paulo <carolinadefpaulo@gmail.com>
2021-03-22 21:43:30 -0300

    feat: new exercise

---
+++ Arrays/Sum_Elements.rb
@@ -0,0 +1,20 @@
+#Sum Elements
+#Write a method sum_elements(arr1, arr2) that takes in two arrays.
+#The method should return a new array containing the results of adding together corresponding elements of the original arrays. You can assume the arrays have the same length.
+
+
+def sum_elements(arr1, arr2)
+  newArray = []
+  i = 0
+  while i < arr1.length 
+    newWord = arr1[i] + arr2[i]
+    newArray << newWord
+    i += 1
+  end
+  return newArray
+
+end
+
+print sum_elements([7, 4, 4], [3, 2, 11])                            # => [10, 6, 15]
+puts
+print sum_elements(["cat", "pizza", "boot"], ["dog", "pie", "camp"]) # => ["catdog", "pizzapie", "bootcamp"]
